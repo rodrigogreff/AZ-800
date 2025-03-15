0e723a41e1ff52baaff7f9429f073d9d8afb4d2c
Rodrigo Greff <67062452+rodrigogreff@users.noreply.github.com>
2025-03-14 23:52:55 -0300

    Update Install-ADDSDomainController.ps1

--- Install-ADDSDomainController.ps1
+++ Install-ADDSDomainController.ps1
@@ -1,19 +1,32 @@
-#
-# Windows PowerShell script for AD DS Deployment
-#
+# Windows PowerShell script for AD DS Additional Domain Controller Deployment
 
-Import-Module ADDSDeployment
+# Variáveis
+$DomainName = "rodrigogreff.cloud"
+$DatabasePath = "C:\Windows\NTDS"
+$LogPath = "C:\Windows\NTDS"
+$SysVolPath = "C:\Windows\SYSVOL"
+$featureLogPath = "C:\poshlog\featurelog.txt"
+$Password = (ConvertTo-SecureString -String "P@ssw0rd" -AsPlainText -Force)
+
+# Instalar os recursos necessários: AD DS, DNS e GPMC
+Start-Job -Name addFeature -ScriptBlock { 
+    Add-WindowsFeature -Name "ad-domain-services" -IncludeAllSubFeature -IncludeManagementTools 
+    Add-WindowsFeature -Name "dns" -IncludeAllSubFeature -IncludeManagementTools 
+    Add-WindowsFeature -Name "gpmc" -IncludeAllSubFeature -IncludeManagementTools 
+} 
+Wait-Job -Name addFeature 
+Get-WindowsFeature | Where-Object { $_.Installed } >> $featureLogPath
+
+# Promover o servidor como um Controlador de Domínio Adicional
 Install-ADDSDomainController `
--NoGlobalCatalog:$false `
--CreateDnsDelegation:$false `
--Credential (Get-Credential) `
--CriticalReplicationOnly:$false `
--DatabasePath "C:\Windows\NTDS" `
--DomainName "rodrigogreff.cloud" `
--InstallDns:$true `
--LogPath "C:\Windows\NTDS" `
--NoRebootOnCompletion:$false `
--SiteName "Filial" `
--SysvolPath "C:\Windows\SYSVOL" `
--Force:$true
+    -DomainName $DomainName `
+    -InstallDns:$true `
+    -Credential (Get-Credential) `
+    -DatabasePath $DatabasePath `
+    -LogPath $LogPath `
+    -SysvolPath $SysVolPath `
+    -NoGlobalCatalog:$false `
+    -SafeModeAdministratorPassword $Password `
+    -NoRebootOnCompletion:$false `
+    -Force:$true
 
